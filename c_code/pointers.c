6ff59448862d07e14a3d24ae40fea2b9174ac165
sergiogallegos <gallegoshinojosa@gmail.com>
2025-12-15 22:30:23 -0700

    update c code

--- c_code/pointers.c
+++ c_code/pointers.c
@@ -12,7 +12,66 @@ void swap(int *x, int*y) {
     *y = temp;
 }
 
+// TODO: Calculate sum of array of elements using pointing arithmetic
+// Do NOT use array indexing (arr[i]), only use pointers
+int sum_array(int *arr, int size) {
+    int result = 0;
+    for (int i = 0; i < size; i++) {
+        result += *(arr + i);
+    }
+    return result;
+}
+
+// TODO: Return pointer to the largest element in the array
+int* find_max(int *arr, int size) {
+    int *max = arr; // point to the first element
+    for (int i = 1; i < size; i++) {
+        if(*(arr + i) > *max) {
+            max = arr + i; // move pointer
+        }
+    }
+    return max;
+}
+
+// TODO: Reverse array using only pointers (no indexing)
+// Use two pointers: one to start, one at end
+void reverse_array(int *arr, int size) {
+    int *left = arr;
+    int *right = arr + size -1;
+    while (left < right) {
+        int temp = *left;
+        *left = *right;
+        *right = temp;
+        left++;
+        right--;
+    }
+}
+
+void print_array(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    printf("Before: ");
+    print_array(arr, 5);
+    reverse_array(arr, 5);
+    printf("After: ");
+    print_array(arr, 5); // Should print: 5 4 3 2 1
+
+    int numbers2[] = {3, 7, 2, 9, 1};
+    int *max_ptr = find_max(numbers2, 5);
+    printf("Max value: %d\n", *max_ptr); // should print 9
+    printf("Address: %p\n", (void*)max_ptr);
+
+    int numbers[] = {1, 2, 3, 4, 5};
+    int result = sum_array(numbers, 5);
+    printf("Sum: %d\n", result); // Should print 15
+
     //int x = 5;
     //increment(&x); // Pass address of x
     //printf("%d\n", x); // prints 6
